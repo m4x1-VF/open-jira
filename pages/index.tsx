5039b66ccf488311cf86b9a50516ed7f4a3f00c3
Maximiliano Vilariño <mvilarino@aldaba.es>
2024-11-18 12:18:21 +0100

    Creación tarjetas board

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,28 @@
 import { Layout } from "@/components/layouts";
-import { Typography } from "@mui/material";
+import { Card, CardContent, CardHeader } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 
 export default function HomePage() {
   return (
-    <Layout>
-      <Typography variant="h1" color="primary">
-        Welcome to OpenJira!
-      </Typography>
+    <Layout title="Home - OpenJira">
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 12, sm: 4 }}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="Pendientes" />
+            <CardContent></CardContent>
+          </Card>
+        </Grid>
+        <Grid size={{ xs: 12, sm: 4 }}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="En Progreso" />
+          </Card>
+        </Grid>
+        <Grid size={{ xs: 12, sm: 4 }}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="Completadas" />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
