433cd6e6ba43ccd0a9b758f02be671400a674a2b
Maximiliano Vilariño <maxi.vilarino@gmail.com>
2024-11-18 22:14:41 +0100

    Creacion de entrdas y tipado

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,12 +1,33 @@
 import { FC, useReducer } from "react";
+import { v4 as uuidv4 } from "uuid";
 import { EntriesContext, entriesReducer } from "./";
+import { Entry } from "@/interfaces";
 
 export interface EntriesState {
-  entries: [];
+  entries: Entry[];
 }
 
 const Entries_INITIAL_STATE: EntriesState = {
-  entries: [],
+  entries: [
+    {
+      _id: uuidv4(),
+      description: "Crear componente de Navbar",
+      status: "pending",
+      createdAt: Date.now(),
+    },
+    {
+      _id: uuidv4(),
+      description: "Crear componente de Sidebar",
+      status: "in-progress",
+      createdAt: Date.now() - 1000000,
+    },
+    {
+      _id: uuidv4(),
+      description: "Crear componente de Button",
+      status: "finished",
+      createdAt: Date.now() - 100000,
+    },
+  ],
 };
 
 export const EntriesProvider: FC = ({ children }) => {
