71d07114602ca9d1127d93ab37dc24979b752fe8
Maximiliano Vilariño <mvilarino@aldaba.es>
2024-11-25 15:25:14 +0100

    Leer las entradas desde el Backend

--- context/entries/entriesReducer.ts
+++ context/entries/entriesReducer.ts
@@ -3,7 +3,8 @@ import { EntriesState } from "./";
 
 type EntriesType =
   | { type: "[Entry] Add-Entry"; payload: Entry }
-  | { type: "[Entry] Entry-Updated"; payload: Entry };
+  | { type: "[Entry] Entry-Updated"; payload: Entry }
+  | { type: "[Entry] Refresh-Data"; payload: Entry[] };
 
 export const entriesReducer = (
   state: EntriesState,
@@ -22,6 +23,11 @@ export const entriesReducer = (
           }
           return entry;
         }),
+      };
+    case "[Entry] Refresh-Data":
+      return {
+        ...state,
+        entries: [...action.payload],
       };
     default:
       return state;
