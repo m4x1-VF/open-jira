5abf713826ffae573f3527d9ec4a981b6beb2b06
Maximiliano Vilariño <mvilarino@aldaba.es>
2024-11-25 09:44:03 +0100

    Creación endpoint obtener entradas

---
+++ pages/api/entries/index.ts
@@ -0,0 +1,25 @@
+import { db } from "@/database";
+import { Entry, IEntry } from "@/models";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = { message: string } | IEntry[];
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  switch (req.method) {
+    case "GET":
+      return getEntries(res);
+
+    default:
+      return res.status(400).json({ message: "Enpoint not found" });
+  }
+}
+
+const getEntries = async (res: NextApiResponse<Data>) => {
+  await db.connect();
+  const entries = await Entry.find().sort({ createdAt: "ascending" });
+  await db.disconnect();
+  res.status(200).json(entries);
+};
