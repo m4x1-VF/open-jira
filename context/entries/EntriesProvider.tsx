20c2e926aa039304bc30c2e76147743a3d539430
Maximiliano Vilariño <mvilarino@aldaba.es>
2024-11-27 10:03:03 +0100

    Crear entrada desde el front

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,5 +1,5 @@
 import { FC, useEffect, useReducer } from "react";
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
 import { EntriesContext, entriesReducer } from "./";
 import { Entry } from "@/interfaces";
 import { entriesApi } from "@/apis";
@@ -14,14 +14,15 @@ const Entries_INITIAL_STATE: EntriesState = {
 
 export const EntriesProvider: FC = ({ children }) => {
   const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE);
-  const addNewEntry = (description: string) => {
-    const newEntry: Entry = {
-      _id: uuidv4(),
-      description,
-      status: "pending",
-      createdAt: Date.now(),
-    };
-    dispatch({ type: "[Entry] Add-Entry", payload: newEntry });
+  const addNewEntry = async (description: string) => {
+    // const newEntry: Entry = {
+    //   _id: uuidv4(),
+    //   description,
+    //   status: "pending",
+    //   createdAt: Date.now(),
+    // };
+    const { data } = await entriesApi.post<Entry>("/entries", { description });
+    dispatch({ type: "[Entry] Add-Entry", payload: data });
   };
   const updateEntry = (entry: Entry) => {
     dispatch({ type: "[Entry] Entry-Updated", payload: entry });
